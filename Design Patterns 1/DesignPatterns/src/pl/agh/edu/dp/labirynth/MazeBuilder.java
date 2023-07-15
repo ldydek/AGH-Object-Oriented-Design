683eff90a1987c947e8d451bffa58d6de0e71506
ldydek <91955435+ldydek@users.noreply.github.com>
2023-07-15 18:13:03 +0200

    Delete Design Patterns 1/DesignPatterns directory

--- Design Patterns 1/DesignPatterns/src/pl/agh/edu/dp/labirynth/MazeBuilder.java
+++
@@ -1,7 +0,0 @@
-package pl.agh.edu.dp.labirynth;
-
-public abstract class MazeBuilder {
-    abstract void addRoom(Room room);
-    abstract void addDoor(Room r1, Room r2) throws Exception;
-    abstract void commonWall(Room r1, Room r2);
-}
