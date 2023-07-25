9500e954f8be441487bfc831b142973c364c7124
ldydek <lukaszdydek72@gmail.com>
2023-07-25 15:30:39 +0200

    testing adapter implementation

--- Design Patterns 3/DesignPatterns3/src/main/java/org/example/Main.java
+++ Design Patterns 3/DesignPatterns3/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.adapter.RoundHole;
+import org.example.adapter.SquarePeg;
+import org.example.adapter.SquarePegAdapter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        RoundHole roundHole = new RoundHole(5);
+
+        SquarePeg smallSquarePeg = new SquarePeg(5);
+        SquarePeg largeSquarePeg = new SquarePeg(10);
+
+        SquarePegAdapter smallSquarePegAdapter = new SquarePegAdapter(smallSquarePeg);
+        SquarePegAdapter largeSquarePegAdapter = new SquarePegAdapter(largeSquarePeg);
+
+        System.out.println(roundHole.fits(smallSquarePegAdapter));
+        System.out.println(roundHole.fits(largeSquarePegAdapter));
     }
 }
